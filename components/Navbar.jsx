6557099798494a1d3cccad1acdf992446be04d77
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-24 12:18:26 +0530

    s2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,18 @@
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <>
+            <div id="navbar">
+                <div className="left">
+                    <h1>TASPAY</h1>
+                </div>
+                <div className="right">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </div>
+            </div>
+        </>
     );
 }
 
