85963eba0bfaeea985057ae4f8068e94e9dddd1a
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-24 12:45:30 +0530

    s2

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,7 +1,22 @@
 const Home = () => {
     return (
         <div>
-            Home
+            <div id="home">
+                <div className="container">
+                    <div className="left">
+                        <h1>Online Payment Made Easy For You</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, quia quis? Ipsa officia ad deserunt voluptate quam, nisi odio officiis tempora recusandae voluptate quam, nisi odio officiis tempora recusandae</p>
+                        <h3>Get early access for you</h3>
+                        <form>
+                            <input type="text" placeholder="Enter Your Email"/>
+                            <button type="submit"></button>
+                        </form>
+                    </div>
+                </div>
+                <div className="right">
+
+                </div>
+            </div>
         </div>
     );
 }
