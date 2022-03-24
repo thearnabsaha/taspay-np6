f03a3b5d9d3763b503ac283ae3a0575388bbec22
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-24 12:33:01 +0530

    s2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,14 +2,16 @@ const Navbar = () => {
     return (
         <>
             <div id="navbar">
-                <div className="left">
-                    <h1>TASPAY</h1>
-                </div>
-                <div className="right">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
+                <div className="container">
+                    <div className="left">
+                        <h1>Taspay</h1>
+                    </div>
+                    <div className="right">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Services</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </div>
                 </div>
             </div>
         </>
