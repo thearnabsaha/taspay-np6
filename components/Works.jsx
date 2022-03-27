f600192b073d1f66aa7670a25f1d50e5a77ac540
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 11:29:36 +0530

    s4

--- components/Works.jsx
+++ components/Works.jsx
@@ -20,16 +20,6 @@ const Works = () => {
                             </div>
                         </>
                     })}
-                        {/* <div className="content">
-                            <div className="icons"><MdDownload className="icon"/></div>
-                            <h2>Download App</h2>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
-                        </div>
-                        <div className="content">
-                            <div className="icons"><MdDownload className="icon"/></div>
-                            <h2>Download App</h2>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
-                        </div> */}
                     </div>
                 </div>
             </div>
