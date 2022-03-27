35dca0442ff34a91cf935009151dc3394d7362fd
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 11:16:28 +0530

    s4

--- components/About.jsx
+++ components/About.jsx
@@ -1,14 +1,18 @@
 const About = () => {
     return (
         <>
-            <h3>-- AVAILABLE @ GOOGLE AND IOS APP STORE ONLY</h3>
-            <div className="container">
-                <div className="right">
+            <div id="about">
+                <div className="container">
+                    <h3>-- AVAILABLE @ GOOGLE AND IOS APP STORE ONLY</h3>
+                    <div className="container">
+                        <div className="right">
 
-                </div>
-                <div className="left">
-                    <h1>How to use the App?</h1>
-                    
+                        </div>
+                        <div className="left">
+                            <h1>How to use the App?</h1>
+                            
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
