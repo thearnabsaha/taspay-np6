a55c10ae8553f746396a051d88941571e11a31c7
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 10:07:46 +0530

    s4

--- components/Works.jsx
+++ components/Works.jsx
@@ -1,8 +1,18 @@
+
 const Works = () => {
     return (
-        <div>
-            Enter
-        </div>
+        <>
+            <div id="work">
+                <div className="container">
+                    <h1>How does it Work</h1>
+                    <div className="contentContainer">
+
+                        <h1>Downlaod App</h1>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
