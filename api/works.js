198658a564a33e392ca904e322db08f9f3ed626c
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 11:37:41 +0530

    s4

--- api/works.js
+++ api/works.js
@@ -3,15 +3,21 @@ export const works = [
         id: 1,
         title: "Downlaod App",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+        icon:"MdDownload",
     },
     {
         id: 2,
         title: "Complete the Instruction",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+        icon:"MdAttachMoney",
+
     },
     {
         id: 3,
         title: "Receive your funds",
         desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+        icon:"MdOutlineScreenShare",
+
     },
 ];
+// MdDownload , MdAttachMoney, MdOutlineScreenShare
