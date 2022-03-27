e80686d00de2ccb087d0ae4da5b736394604c741
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 18:01:17 +0530

    s4

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,13 @@ import Footer from "../components/Footer";
 import Home from "../components/Home";
 import Services from "../components/Services";
 import Works from "../components/Works";
-
+import Head from 'next/head';
 const index = () => {
   return (
     <>
+    <Head>
+      <title>TasPay</title>
+    </Head>
       <Navbar/>
       <Home/>
       <Works/>
