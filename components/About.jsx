3165539acbaf07fef16d1a261c92d04bfe2b3687
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 18:35:59 +0530

    s4

--- components/About.jsx
+++ components/About.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import { send } from "../api/send";
+import Image from 'next/image';
+import mh from '../public/images/hero3.jpg'
 const About = () => {
     const [sendData, setSendData] = useState(send)
     return (
         <>
             <div id="about">
-                <div className="container">
-                    <h3>-- AVAILABLE @ GOOGLE AND IOS APP STORE ONLY</h3>
+                <div>
                     <div className="container">
                         <div className="right">
-
+                            <Image src={mh} width="300" height="500" objectFit='cover' alt='arnabs image' className="img"/>
                         </div>
                         <div className="left">
+                            <h4>-- AVAILABLE @ GOOGLE AND IOS APP STORE ONLY</h4>
                             <h1>How to use the App?</h1>
                             <div className="contentContainer">
                                 {
@@ -19,8 +21,10 @@ const About = () => {
                                         return <>
                                                 <div className="content" key={e.id}>
                                                     <h3>{e.id}</h3>
-                                                    <h2>{e.title}</h2>
-                                                    <p>{e.desc}</p>
+                                                    <div className="desc">
+                                                        <h2>{e.title}</h2>
+                                                        <p>{e.desc}</p>
+                                                    </div>
                                                 </div>
                                         </>
                                     })
