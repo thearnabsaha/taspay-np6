a462ed0c9f24da4c0dbe44d8fe56e23df1ba8eef
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 10:19:19 +0530

    s2

--- components/Works.jsx
+++ components/Works.jsx
@@ -7,18 +7,18 @@ const Works = () => {
                     <h1>How does it Work</h1>
                     <div className="contentContainer">
                         <div className="content">
-                        <MdDownload/>
-                            <h1>Downlaod App</h1>
+                            <MdDownload className="icon"/>
+                            <h2>Downlaod App</h2>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
                         </div>
                         <div className="content">
-                        <MdDownload/>
-                            <h1>Downlaod App</h1>
+                            <MdDownload className="icon"/>
+                            <h2>Downlaod App</h2>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
                         </div>
                         <div className="content">
-                        <MdDownload/>
-                            <h1>Downlaod App</h1>
+                            <MdDownload className="icon"/>
+                            <h2>Downlaod App</h2>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
                         </div>
                     </div>
