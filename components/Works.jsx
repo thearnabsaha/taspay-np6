588fbb86e55a9a478784ba23f3e1e697823b7249
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-27 11:24:28 +0530

    s4

--- components/Works.jsx
+++ components/Works.jsx
@@ -1,12 +1,27 @@
 import { MdDownload } from "react-icons/md";
+import { useState } from "react";
+import { works } from "../api/works";
+
 const Works = () => {
+    const [workData, setWorkData] = useState(works)
+    console.log(workData);
     return (
         <>
             <div id="work">
                 <div className="container">
                     <h1>How does it Work</h1>
                     <div className="contentContainer">
-                        <div className="content">
+                    {workData.map((e)=>{
+                        return <>
+                            <div className="content">
+                                <div className="icons"><MdDownload className="icon"/></div>
+                                <h2>Download App</h2>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
+                            </div>
+                        </>
+                    })}
+                        
+                        {/* <div className="content">
                             <div className="icons"><MdDownload className="icon"/></div>
                             <h2>Download App</h2>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
@@ -15,12 +30,7 @@ const Works = () => {
                             <div className="icons"><MdDownload className="icon"/></div>
                             <h2>Download App</h2>
                             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
-                        </div>
-                        <div className="content">
-                            <div className="icons"><MdDownload className="icon"/></div>
-                            <h2>Download App</h2>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?</p>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
             </div>
