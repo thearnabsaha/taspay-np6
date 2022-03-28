63fc3c4cda7c8123a860661bf4cc5f6691dccb58
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-28 11:39:18 +0530

    s5

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,7 +6,9 @@ const Contact = () => {
             <div id="contact">
                 <div id="container">
                     <div className="left">
-
+                        <h1></h1>
+                        <p></p>
+                        <Image src={mh} width="500" height="500" objectFit='cover' alt='arnabs image'/>
                     </div>
                     <div className="right">
 
