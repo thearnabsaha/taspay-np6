9f1ebd20d148ba2a5a2ec070351e18a74d728028
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-28 09:58:34 +0530

    s5

---
+++ api/services.js
@@ -0,0 +1,43 @@
+export const services = [
+    {
+        id: 1,
+        title: "Register for free.",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, Etquia quis?Lorem ipsum, Etquia quis?",
+        icon:"MdDownload",
+    },
+    {
+        id: 2,
+        title: "Verify your identity",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+        icon:"MdAttachMoney",
+
+    },
+    {
+        id: 3,
+        title: "Add recipient's bank details.",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+        icon:"MdOutlineScreenShare",
+
+    },
+    {
+        id: 4,
+        title: "Pay for your transfer.",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+        icon:"MdOutlineScreenShare",
+
+    },
+    {
+        id: 5,
+        title: "choose an amount to send.",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+        icon:"MdOutlineScreenShare",
+
+    },
+    {
+        id: 6,
+        title: "That's it.",
+        desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+        icon:"MdOutlineScreenShare",
+
+    },
+];
--- components/Services.jsx
+++ components/Services.jsx
@@ -1,7 +1,22 @@
+import { services } from "../api/services";
 const Services = () => {
     return (
         <>
-            
+            <div id="services">
+                    <h1>How to send money</h1>
+                <div className="container">
+                    {
+                        services.map((e)=>{
+                            return <>
+                                <div className="content" key={e.id}>
+                                    <h1>{e.title}</h1>
+                                    <p>{e.desc}</p>
+                                </div>
+                            </>
+                        })
+                    }
+                </div>
+            </div>
         </>
     );
 }
