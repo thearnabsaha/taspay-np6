14845919e6076876ae5bedd6e02fc3113081beba
thearnabsaha <thearnabsaha201@gmail.com>
2022-03-28 11:37:46 +0530

    s5

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,9 +1,16 @@
+import mh from '../public/images/mainHero.png'
+import Image from 'next/image';
 const Contact = () => {
     return (
         <>
             <div id="contact">
                 <div id="container">
-                    
+                    <div className="left">
+
+                    </div>
+                    <div className="right">
+
+                    </div>
                 </div>
             </div>
         </>
